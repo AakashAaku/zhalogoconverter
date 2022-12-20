b2912d705e47578e8d7dc850bdf0065099fe1e41
AakashAaku <guragaiaakash@gmail.com>
2022-12-20 14:08:20 +0545

    Add files via upload
    file Changes

---
+++ src/i18n/languages.js
@@ -0,0 +1,47 @@
+/**
+ * SVGcode—Convert raster images to SVG vector graphics
+ * Copyright (C) 2022 Google LLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+const languages = {
+  arTN: 'عربى',
+  beBY: 'Беларуская',
+  caES: 'Català',
+  daDK: 'Dansk',
+  deDE: 'Deutsch',
+  elGR: 'Ελληνικά',
+  enAU: 'English (Australia) 🦘',
+  enGB: 'English (United Kingdom)',
+  enUS: 'English (United States)',
+  esES: 'Español',
+  frFR: 'Français',
+  heIL: 'עברית',
+  idID: 'Bahasa Indonesia',
+  itIT: 'Italiano',
+  jaJP: '日本語',
+  koKR: '한국어',
+  nlNL: 'Nederlands',
+  noNO: 'Norsk',
+  plPL: 'Polski',
+  ptBR: 'Português',
+  ruRU: 'Русский',
+  ukUA: 'Українська',
+  zhCN: '中文',
+};
+
+// ignore unused exports default
+export default languages;
